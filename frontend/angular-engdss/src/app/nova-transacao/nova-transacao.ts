5aa1ce71805f0329b84d4935782165e2a4c74891
Luiz Henrique <luizhengs@gmail.com>
2026-03-24 00:41:22 -0300

    Tela super básica de Nova Transação está concluída (protótipo)

--- frontend/angular-engdss/src/app/nova-transacao/nova-transacao.ts
+++ frontend/angular-engdss/src/app/nova-transacao/nova-transacao.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { FormsModule } from '@angular/forms';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { ToastModule } from 'primeng/toast';
+import { PanelModule } from 'primeng/panel';
 
+
+interface nova_transacao_form {
+  chave: string | null;
+  valor: number | null;
+  descricao: string | null;
+}
 @Component({
   selector: 'app-nova-transacao',
-  imports: [],
+  imports: [PanelModule, ToastModule, InputTextModule, InputNumberModule, ButtonModule, FloatLabelModule, FormsModule],
   templateUrl: './nova-transacao.html',
   styleUrl: './nova-transacao.css',
+  providers: [MessageService]
 })
 export class NovaTransacao {
+
+  form: nova_transacao_form = {
+    chave: null,
+    valor: null,
+    descricao: null
+  }
+
+  constructor(private messageService: MessageService) {}
+  
+  enviar_transacao() {
+    this.messageService.add({ severity: 'success', summary: 'Transação Enviada', detail: `Valor: ${this.form.valor}, Chave: ${this.form.chave}, Descrição: ${this.form.descricao}` });
+  }
 
 }
