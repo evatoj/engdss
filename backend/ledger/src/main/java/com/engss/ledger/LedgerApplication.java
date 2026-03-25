5be32dd37badb1bec443ac08e464fcbea4cd5057
João Cirilo <jvscirilo12@gmail.com>
2026-03-25 11:42:12 -0300

    Adicionado Scheduling para quarentena

--- backend/ledger/src/main/java/com/engss/ledger/LedgerApplication.java
+++ backend/ledger/src/main/java/com/engss/ledger/LedgerApplication.java
@@ -2,12 +2,14 @@ package com.engss.ledger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LedgerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LedgerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LedgerApplication.class, args);
+    }
 
 }
