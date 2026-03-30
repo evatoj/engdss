505da5b7e229ff423bb9c7173f874dafe8bd4ac4
Luiz Henrique <luizhengs@gmail.com>
2026-03-30 18:19:12 -0300

    Conexão através do host.docker.internal para o rabbitmq e o postgres

--- docker/postgresql/init/02-seed.sql
+++ docker/postgresql/init/02-seed.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuario (nome, saldo_disponivel, saldo_pendente)
+INSERT INTO transaction.usuario (nome, saldo_disponivel, saldo_pendente)
 VALUES ('João', 1000.00, 0.00);
 
-INSERT INTO usuario (nome, saldo_disponivel, saldo_pendente)
+INSERT INTO transaction.usuario (nome, saldo_disponivel, saldo_pendente)
 VALUES ('Maria', 500.00, 0.00);
